a12414129bb0ce87ae9e1bc30196ec54d29d8089
SPRk <SUPRIMEkair@gmail.com>
2022-08-07 05:22:47 +0700

    Initial commit

---
+++ FKG.go
@@ -0,0 +1,11 @@
+package FKG
+
+func GiveMeStr() string {
+	stri := " GiveMeStr - OK"
+	return stri
+}
+
+func GoHELLGo() string {
+	stri2 := " FK YEAH"
+	return stri2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module FKG
+
+go 1.18
